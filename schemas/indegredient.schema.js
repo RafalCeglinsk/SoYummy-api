84e54b8c6f96543596ba0f159f175f53c1286517
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-02-17 16:57:55 +0100

    update schema: idengredient

--- schemas/indegredient.schema.js
+++ schemas/indegredient.schema.js
@@ -1,17 +1,22 @@
 import { Schema, model } from "mongoose"
 
-const IndegredientsSchema = new Schema ({
-    name: {
-        type: String
-    },
-    desc: {
-        type:String
-    },
-    img: {
-        type:String
-    }
+const IndegredientsSchema = new Schema({
+  ttl: {
+    type: String,
+    required: [true, 'Recipe title is required'],
+  },
+  desc: {
+    type: String,
+    require: [true, 'Recipe description is required'],
+  },
+  t: {
+    type: String,
+    default: '',
+  },
+  thb: {
+    type: String,
+    required: [true, 'Set image'],
+  },
 })
 
-const IndegredientsModel = model("Indegredient", IndegredientsSchema)
-
-export default IndegredientsModel
+export const IndegredientsModel = model("Indegredient", IndegredientsSchema)
