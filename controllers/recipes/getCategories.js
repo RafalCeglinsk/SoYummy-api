87c46e75a6d61ac6f9b15697503d969e4d160b6a
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-02-19 03:30:55 +0100

    return sorted category list

--- controllers/recipes/getCategories.js
+++ controllers/recipes/getCategories.js
@@ -1,3 +1,33 @@
-export const getCategories = async (req, res, next) => {
+import { sortCategory } from '../../services/recipes.service.js'
+
+const CATEGORY_LIST = [
+  'Pork',
+  'Breakfast',
+  'Dessert',
+  'Vegetarian',
+  'Side',
+  'Goat',
+  'Lamb',
+  'Pasta',
+  'Chicken',
+  'Seafood',
+  'Starter',
+  'Vegan',
+  'Miscellaneous',
+  'Beef',
+]
+
+export const getCategories = async (_, res, next) => {
+  try {
+    const sortedCategory = sortCategory(CATEGORY_LIST)
+
+    return res.status(200).json({
+      code: 200,
+      status: `OK`,
+      categories: sortedCategory,
+    })
     
+  } catch (error) {
+    next(error)
+  }
 }
---
+++ services/recipes.service.js
@@ -0,0 +1,3 @@
+export const sortCategory = (data) => {
+  return data.sort()
+}
