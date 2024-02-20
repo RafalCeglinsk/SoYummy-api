face7dffeb0850d17ae0861d650a44992660d701
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-02-20 18:35:14 +0100

    Ingredient Search done

--- routes/api/ingredients.js
+++ routes/api/ingredients.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import { ingredientList } from '../../controllers/ingredients/ingredientList.js'
+import { ingredientSearch } from '../../controllers/ingredients/ingredientSearch.js'
+import { auth } from '../../middlewares/auth.js'
 
 const router = express.Router()
 
-router.get('/', ingredientList)
+router.get('/list', ingredientList)
+router.get('/', auth, ingredientSearch)
 
 export { router }
--- services/ingredients.service.js
+++ services/ingredients.service.js
@@ -1,5 +1,30 @@
 import { Ingredient } from '../schemas/ingredient.schema.js'
+import { Recipe } from '../schemas/recipe.schema.js'
+import { Types } from 'mongoose'
 
-export const getIngredientsList = async (i) => {
+export const getIngredientsList = async () => {
   return Ingredient.find({})
+}
+
+export const getIngredientsSearch = async (ingredients) => {
+  const searchIngredient = await Ingredient.find({
+    ttl: { $regex: new RegExp(`^${ingredients}`, 'i') },
+  })
+
+  const idIngredient = searchIngredient.map((i) => i._id)
+
+  const result = await Recipe.find({
+    ingredients: {
+      $elemMatch: {
+        id: {
+          $in: [
+            new Types.ObjectId(idIngredient[0]),
+            new Types.ObjectId(idIngredient[1]),
+          ],
+        },
+      },
+    },
+  })
+
+  return result
 }
