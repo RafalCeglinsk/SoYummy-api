744646ba2affeed8a94e121a919e77fb6e0cdcd1
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-02-21 03:24:29 +0100

    an endpoint for receiving products from the user's shopping list.

--- services/shoppingList.service.js
+++ services/shoppingList.service.js
@@ -15,3 +15,14 @@ export const addIngredients = async (user, data) => {
 
   return result
 }
+
+export const removeIngredients = async () => {}
+
+export const displayIngredients = async (_id) => {
+  // display only array shoppingList
+  const { shoppingList } = await User.findById(_id).populate({
+    path: 'shoppingList.ingredientId',
+    ref: 'ingredients',
+  })
+  return shoppingList
+}
