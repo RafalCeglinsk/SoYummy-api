6df6194429b9f0085422f2fbaa3dc120cc8d7111
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-02-21 12:24:22 +0100

    Title Search done

--- controllers/search/search.js
+++ controllers/search/search.js
@@ -1,3 +1,17 @@
+import { getTitleSearch } from "../../services/search.service.js"
+
 export const search = async (req, res, next) => {
-    
+      try {
+        const { title = '' } = req.query
+
+        const result = await getTitleSearch(title)
+
+        return res.status(200).json({
+          code: 200,
+          status: `OK`,
+          result,
+        })
+      } catch (error) {
+        next(error)
+      }
 }
