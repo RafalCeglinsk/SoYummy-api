26defa77de338aadb4c2dff08a753fc915292305
Krzysztof Kryczka <kw.kryczka@gmail.com>
2024-02-24 23:13:24 +0100

    Changed diskStorage to memoryStorage

--- controllers/ownRecipes/addRecipe.js
+++ controllers/ownRecipes/addRecipe.js
@@ -3,6 +3,9 @@ import { ownRecipesSchema } from '../../schemas/recipe.schema.js'
 import { createRecipe } from '../../services/ownRecipes.service.js'
 import { Types } from 'mongoose'
 import { uploadRecipeImage } from '../../utils/cloudinary.js'
+import Datauri from 'datauri'
+import path from 'path'
+
 export const addRecipe = async (req, res, next) => {
   const validationResult = ownRecipesSchema.validate(req.body)
   if (validationResult.error) {
@@ -11,16 +14,40 @@ export const addRecipe = async (req, res, next) => {
 
   try {
     const { _id } = req.user
-    const { title, category, instructions, description, time, ingredients } =
-      req.body
+    const { title, category, instructions, description, time } = req.body
     let recipeImg = null
+
+    const ingredients = [
+      {
+        id: '640c2dd963a319ea671e3773', //id produktu z tabeli ingredients
+        measure: '500g',
+      },
+      {
+        id: '640c2dd963a319ea671e3735', //id produktu z tabeli ingredients
+        measure: '1/2 cup',
+      },
+    ]
+
+    console.log(JSON.stringify(req.body))
+    console.log('req.file.mimetype:' + req.file.mimetype)
+
+    /* 
+    Converts the buffer in the parsed file to base64, and further transforms this representation 
+    to a data URI, which is then fed to the handleUpload function to upload. Once it succeeds, 
+    we send the upload response we get from cloudinary back to the client-side, and if it fails, 
+    the error response is sent back.
+    */
+    const b64 = Buffer.from(req.file.buffer).toString('base64')
+    
+    // for example ( -- (req.file.mimetype:image/jpeg)
+    let dataURI = 'data:' + req.file.mimetype + ';base64,' + b64
 
     if (!req.file) {
       // set default image no preview
       recipeImg =
         'https://res.cloudinary.com/dyomlbrcf/image/upload/v1708692584/yummy-api/recipes/linjixv3vn290nvjmf2v.png'
     } else {
-      recipeImg = await uploadRecipeImage(req.file.path)
+      recipeImg = await uploadRecipeImage(dataURI)
     }
 
     let parsedIngredients = null
@@ -32,7 +59,7 @@ export const addRecipe = async (req, res, next) => {
       // This is not an array of objects, so parse it as a JSON string
       parsedIngredients = JSON.parse(ingredients)
     }
-    
+
     const objData = {
       preview: recipeImg.hasOwnProperty('url') ? recipeImg.url : recipeImg,
       thumb: recipeImg.hasOwnProperty('url') ? recipeImg.url : recipeImg,
--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,27 +1,35 @@
-import multer from 'multer'
-import path from 'path'
+import multer, { memoryStorage } from 'multer'
+// import path from 'path'
 
-const tempDir = path.resolve('../temp')
+// const tempDir = path.resolve('../temp')
 
-const multerConfig = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, tempDir)
-  },
-  filename: (req, file, callBack) => {
-    const { originalname } = file
-    const uniquePrefix = Math.round(Math.random() * 1e9)
-    const fileName = `${uniquePrefix}-${originalname}`
-    callBack(null, fileName)
-  },
-})
+// const multerConfig = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, tempDir)
+//   },
+//   filename: (req, file, callBack) => {
+//     const { originalname } = file
+//     const uniquePrefix = Math.round(Math.random() * 1e9)
+//     const fileName = `${uniquePrefix}-${originalname}`
+//     callBack(null, fileName)
+//   },
+// })
+
+const multerConfig = multer.memoryStorage()
 
 export const upload = multer({
   storage: multerConfig,
   fileFilter: (req, file, callBack) => {
     if (file.mimetype.includes('image')) {
       callBack(null, true)
-      return
+    } else {
+      callBack(new Error('Invalid file format. Send only iamges.'))
     }
-    callBack(null, false)
+  },
+  filename: (req, file, callBack) => {
+    const { originalname } = file
+    const uniquePrefix = Math.round(Math.random() * 1e9)
+    const fileName = `${uniquePrefix}-${originalname}`
+    callBack(null, fileName)
   },
 })
