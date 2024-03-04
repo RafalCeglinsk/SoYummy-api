83042bac317cd31d7ad3982eda912ac216309b66
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-03-04 11:41:43 +0100

    Created function of getAllRecipes

--- controllers/recipes/getAllRecipes.js
+++ controllers/recipes/getAllRecipes.js
@@ -1,3 +1,11 @@
+import { Recipe } from "../../schemas/recipe.schema.js";
+
 export const getAllRecipes = async (req, res, next) => {
-    
-}
+  try {
+    const allRecipes = await Recipe.find();
+    return res.status(200).json(allRecipes);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
