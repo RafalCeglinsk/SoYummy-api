4a7d3657218433cc11e860f462c13a4a5f6891a3
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-05-01 11:48:39 +0200

    Updatge popularRecipes.service.js

--- services/popularRecipes.service.js
+++ services/popularRecipes.service.js
@@ -1,19 +1,20 @@
-import { Recipe } from '../schemas/recipe.schema.js'
+import { Recipe } from "../schemas/recipe.schema.js";
 
 export const getPopularRecipes = async (page, limit) => {
   const recipes = await Recipe.aggregate([
-    { $match: { 'favorites.0': { $exists: true } } },
-    { $addFields: { favoriteCount: { $size: '$favorites' } } },
+    { $match: { "favorites.0": { $exists: true } } },
+    { $addFields: { favoriteCount: { $size: "$favorites" } } },
     { $sort: { favoriteCount: -1 } },
     {
       $facet: {
         recipes: [{ $skip: page * limit - limit }, { $limit: limit }],
-        count: [{ $count: 'total' }],
+        count: [{ $count: "total" }],
       },
     },
     {
       $project: {
         recipes: {
+          _id: 1,
           title: 1,
           category: 1,
           instructions: 1,
@@ -21,11 +22,11 @@ export const getPopularRecipes = async (page, limit) => {
           thumb: 1,
           favorites: 1,
         },
-        total: { $arrayElemAt: ['$count.total', 0] },
+        total: { $arrayElemAt: ["$count.total", 0] },
         page: { $literal: page },
         limit: { $literal: limit },
       },
     },
-  ]).exec()
-  return recipes[0]
-}
+  ]).exec();
+  return recipes[0];
+};
