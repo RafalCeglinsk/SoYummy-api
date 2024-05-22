3771db78620462eb1995603239706e6512caff47
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2024-05-22 17:43:33 +0200

    Added pagination handling logic

--- controllers/ownRecipes/getRecipes.js
+++ controllers/ownRecipes/getRecipes.js
@@ -1,22 +1,20 @@
-import { showRecipes } from '../../services/ownRecipes.service.js'
-import { ApiError } from '../../utils/errors/ApiError.js'
-
 export const getRecipes = async (req, res, next) => {
   try {
-    const { _id } = req.user
+    const { _id } = req.user;
+    const { limit = 4, page = 1 } = req.query;
 
-    const result = await showRecipes(_id)
+    const result = await showRecipes(_id, Number(limit), Number(page));
 
     if (result.length === 0) {
-      return next(ApiError.notFound('Not found own recipes'))
+      return next(ApiError.notFound("Not found own recipes"));
     }
 
     return res.status(200).json({
       code: 200,
       status: `OK`,
       result,
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
--- services/ownRecipes.service.js
+++ services/ownRecipes.service.js
@@ -1,19 +1,20 @@
-import { Recipe } from '../schemas/recipe.schema.js'
+import { Recipe } from "../schemas/recipe.schema.js";
 
 export const createRecipe = async (data) => {
-  const recipe = await Recipe.create(data)
-  return recipe
-}
+  const recipe = await Recipe.create(data);
+  return recipe;
+};
 
-export const showRecipes = async (_id) => {
-  const result = await Recipe.find({ owner: _id })
-  return result
-}
+export const showRecipes = async (_id, limit = 10, page = 1) => {
+  const skip = (page - 1) * limit;
+  const result = await Recipe.find({ owner: _id }).limit(limit).skip(skip);
+  return result;
+};
 
 export const deleteRecipe = async (recipeID, _id) => {
   const result = await Recipe.findByIdAndDelete({
     _id: recipeID,
     owner: _id,
-  })
-  return result
-}
+  });
+  return result;
+};
